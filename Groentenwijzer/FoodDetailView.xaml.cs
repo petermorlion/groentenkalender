3f12eeff9599429fa90d73dcd03a2ee922e26b6e
peter morlion <peter.morlion@live.be>
2013-05-13 17:46:11 +0200

    added page for detail + navigate to it

---
+++ Groentenwijzer/FoodDetailView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using Microsoft.Phone.Controls;
+
+namespace Groentenwijzer
+{
+    public partial class FoodDetailView : PhoneApplicationPage
+    {
+        public FoodDetailView()
+        {
+            InitializeComponent();  
+            Loaded += FoodDetailView_Loaded;
+        }
+
+        private void FoodDetailView_Loaded(object sender, RoutedEventArgs e)
+        {
+            FoodItemTitle.Text = NavigationContext.QueryString["FoodName"];
+        }
+    }
+}
